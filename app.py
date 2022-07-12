858fc3282d44c74f02d5aad1684628fe8de7c28f
Matthew Dabney <aaronjalberg@gmail.com>
2022-07-11 19:57:23 -0500

    add drinks to menu

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 file = open("drinks.json")
 drinks = json.load(file)["drinks"]
+MAX_PUMPS = 6
 
 def find_drink(drink, drinks):
    for i in drinks:
@@ -22,7 +23,7 @@ def home():
       }
    return render_template('index.html', **templateData)
 
-@app.route("/make/", methods=['POST'])
+@app.route("/make", methods=['POST'])
 def make_drink():
     drink = request.form['drink']
     print("request received to make " + drink)
@@ -31,6 +32,50 @@ def make_drink():
       'drink': find_drink(drink, drinks)
       }
     return render_template('makingdrink.html', **templateData);
+
+@app.route("/add-drink", methods=['POST'])
+def add_drink():
+   new_drink = {}
+   new_drink['name'] = request.form['new-drink-name']
+
+   ingredients = []
+   for i in range(1, MAX_PUMPS + 1):
+      label = 'new-drink-ingredient-' + str(i)
+      if label not in request.form:
+         break
+
+      request.form['new-drink-ingredient-' + str(i)]
+      new_ingredient = {
+         "name": request.form['new-drink-ingredient-' + str(i)],
+         "quantity": request.form['new-drink-quantity-' + str(i)]
+      }
+      
+      ingredients.append(new_ingredient)
+   
+   new_drink['ingredients'] = ingredients
+   drinks.append(new_drink)
+   data = { "drinks": drinks }
+
+   with open('drinks.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False, indent=4)
+
+   templateData = {
+      'drinks': drinks
+      }
+   return render_template('index.html', **templateData)
+
+@app.route("/create-page", methods=['GET', 'POST'])
+def create_page():
+   templateData = {
+      'number_ingredients': int(request.form['number-ingredients'])
+      }
+
+   return render_template('createpage.html', **templateData)
+
+
+
+
+
 
 
 if __name__ == "__main__":
